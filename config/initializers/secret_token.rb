399ab1c2abb3bd59fa30d64879c39e0c6e78efc9
Rajarshi Nigam <rajarshi.nigam@lmco.com>
2012-07-07 01:38:58 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedditGuesses::Application.config.secret_token = '5023c65839cac73998b1df1f0359171cb5f7c1e567def3670e78e3cf89fc20295c9967e681f1af29cac2ddace09681b544611f301b3f80e9fb8128c4264a87c3'
